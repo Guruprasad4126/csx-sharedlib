8d63ecc8e3e41040a90639d45631f647d2caebaa
Guruprasad4126 <109950659+Guruprasad4126@users.noreply.github.com>
2022-12-20 10:49:21 +0530

    Create testauto.groovy

---
+++ vars/testauto.groovy
@@ -0,0 +1,32 @@
+def call(String No_of_threads = 'a', String Environment = 'a') {
+
+
+pipeline {
+  environment { 
+		Number_of_threads = "${No_of_threads}"
+    Environment       = "${Environment}"
+	}
+    agent any
+   
+    stages {
+        stage ('Functional test'){
+            steps {
+                echo 'Test Completed in success in $Environment'
+                sleep 5
+                }
+            }
+        stage('Integration test') {
+                    steps {
+                        echo 'Test Completed in success in $Environment'
+                        sleep 10
+                        }
+                    }
+       
+        stage ('Security test'){
+            steps {
+                echo 'Selenium Test Completed in success in $Environment'
+                sleep 5
+                }
+            }
+        }    
+    }
